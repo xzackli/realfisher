8197e46db7f3a01bb3115939a972cc559de768b7
xzackli <xzackli@gmail.com>
2022-06-08 22:47:59 -0500

    working fisher notebook

--- fisheranalysis.jl
+++ fisheranalysis.jl
@@ -4,57 +4,118 @@
 using Markdown
 using InteractiveUtils
 
-# ╔═╡ c19e823c-5ee2-4c5e-882f-8611c6decb87
+# ╔═╡ 3707bc3c-e79d-11ec-2411-cdcf2d2bb7fc
 begin
 	using PyCall
 	using PyPlot
 	PyPlot.svg(true)
 	np = pyimport("numpy")
-end
+end;
 
-# ╔═╡ f2a3807f-271c-4049-bb20-46b410bdd5d2
-using ForwardDiff
+# ╔═╡ 5ee55741-2464-4a03-b357-c1fe00136dbf
+using ForwardDiff, LinearAlgebra
 
-# ╔═╡ 4dbb0982-c4b8-4502-a3ee-02cba9d57cfa
-using LinearAlgebra
+# ╔═╡ 129714fd-46a2-4236-bf90-74b7ed331494
+md"""
+# Exact Fisher Analysis with Automatic Differentiation
 
-# ╔═╡ 7cd43bd0-6a8a-4d7f-add1-53cebecd79de
+This notebook tests the statistical power of ACT vs ACT+Planck, in the configurations reported by Aiola et al. 2020. I do this in a nice simple numerical way with automatic differentiation.
+
+I directly implement two likelihoods below,
+1. DR4 **pyactlike**, containing deep and wide patches
+2. Heather Prince's MOPED compressed Planck 2018 likelihood. 
+
+They're both Gaussian likelihoods, and I have tested that they match the originals to machine-precision. Next, I compute the **Hessian** of the likelihood numerically using forward-mode automatic differentiation. 
+
+```math
+F_{ij} = - \frac{ \partial^2 \mathcal{L}}{\partial \theta_i \, \partial \theta_j}
+```
+
+Armed with the Hessian obtained directly from the likelihoods, we can assess the statistical constraining power of ACT vs. ACT + Planck using a traditional Fisher analysis. I present some demonstrations that this test matches the parameter constraints provided in Aiola et al. 2020 to within ~20% (and for most parameters, significantly better!). 
+
+"""
+
+# ╔═╡ 87b65bd2-aec5-407a-b854-67418f48957c
+md"""
+## Likelihoods
+You can skip the next few cells, which essentially just implement pyactlike twice.
+"""
+
+# ╔═╡ 2c4ef3fd-c8d0-451c-86f6-ee7cd7a40025
 begin
-	filename = "../pyactlike/pyactlike/data/bf_ACTPol_WMAP_lcdm.minimum.theory_cl"
-	const tt_lmax = 5000
-	const lmax_win = 7925
-	cl = np.genfromtxt(
-		filename,
-		delimiter=nothing,
-		unpack=false,
-		max_rows=tt_lmax - 1,
-		usecols=(0, 1, 2, 3))
-	ell, dell_tt, dell_te, dell_ee = cl[:,1], cl[:,2], cl[:,3], cl[:,4]
-
-	cltt, clte, clee = zeros(lmax_win), zeros(lmax_win), zeros(lmax_win)
-	l_list = 2:tt_lmax
-	invdlfac = (2π) ./ l_list ./ (l_list .+ 1)
-	cltt[2:tt_lmax] .= dell_tt[begin:(tt_lmax - 1)] .* invdlfac
-	clte[2:tt_lmax] .= dell_te[begin:(tt_lmax - 1)] .* invdlfac
-	clee[2:tt_lmax] .= dell_ee[begin:(tt_lmax - 1)] .* invdlfac
+	struct PlanckLike{T}
+		blmin_TT::Vector{Int}
+		blmax_TT::Vector{Int}
+		bin_w_TT::Vector{T}
+		blmin::Vector{Int}
+		blmax::Vector{Int}
+		bin_w::Vector{T}
+		X_data::Vector{T}
+		fisher::Matrix{T}
+	end
+	
+	struct ACTLike{T}
+		win_d_tt::Matrix{T}
+		win_d_te::Matrix{T}
+		win_d_ee::Matrix{T}
+		win_w_tt::Matrix{T}
+		win_w_te::Matrix{T}
+		win_w_ee::Matrix{T}
+		fisher::Matrix{T}
+		X_data::Vector{T}
+		lmax_win::Int
+	end
+	
+	function like(L::ACTLike, cltt, clte, clee)
+		yp2 = 1.003
+		lmax_win = L.lmax_win
+		cl_tt_d = L.win_d_tt * cltt[2:lmax_win]
+		cl_te_d = L.win_d_te * clte[2:lmax_win]
+		cl_ee_d = L.win_d_ee * clee[2:lmax_win]
+		cl_tt_w = L.win_w_tt * cltt[2:lmax_win]
+		cl_te_w = L.win_w_te * clte[2:lmax_win]
+		cl_ee_w = L.win_w_ee * clee[2:lmax_win]
+		
+		b0 = 5
+		nbintt = 40
+		nbinte = 45
+		nbinee = 45
+		nbin = 260
+	
+		X_model = [
+			cl_tt_d[b0+1 : b0 + nbintt]
+			cl_te_d[begin:nbinte] * yp2
+			cl_ee_d[begin:nbinee] * yp2 * yp2
+			cl_tt_w[(b0+1) : (b0 + nbintt)]
+			cl_te_w[begin:nbinte] * yp2
+			cl_ee_w[begin:nbinee] * yp2 * yp2
+		]
+		
+		Y = L.X_data .- X_model
+		return -0.5 * (Y' * L.fisher * Y)
+	end
+	
+	function like(L::PlanckLike, cltt, clte, clee)
+		ellmin, nbintt, nbinte, nbinee = 2, 217, 199, 199
+		plmin_TT, plmin = 2, 30
+		calPlanck = 1
+		Cltt_bin = [sum(cltt[
+				(L.blmin_TT[i] .+ plmin_TT .- ellmin .+ 1):(L.blmax_TT[i] .+ plmin_TT .+ 1 .- ellmin)] .* L.bin_w_TT[(L.blmin_TT[i] + 1):L.blmax_TT[i]+1]) for i in 1:nbintt]
+		Clte_bin = [sum(clte[
+			(L.blmin[i] .+ plmin .- ellmin .+ 1):(L.blmax[i] .+ plmin .+ 1 .- ellmin)] .* L.bin_w[(L.blmin[i] + 1):L.blmax[i]+1]) for i in 1:nbinte]
+		Clee_bin = [sum(clee[
+			(L.blmin[i] .+ plmin .- ellmin .+ 1):(L.blmax[i] .+ plmin .+ 1 .- ellmin)] .* L.bin_w[(L.blmin[i] + 1):L.blmax[i]+1]) for i in 1:nbinee]
+		X_model = [
+			Cltt_bin ./ (calPlanck^2)
+			Clte_bin ./ (calPlanck^2)
+			Clee_bin ./ (calPlanck^2)
+		]
+		Y = L.X_data .- X_model
+		return -0.5 * (Y' * L.fisher * Y)
+	end
 end
 
-# ╔═╡ c3de8359-835a-461e-8b3c-a6937721dbe4
-size(cltt) # starts with ell = 1. index = ell
-
-# ╔═╡ e4aeb22f-32b0-40fe-bc24-0c1125682aaa
-struct ACTLike{T}
-	win_d_tt::Matrix{T}
-	win_d_te::Matrix{T}
-	win_d_ee::Matrix{T}
-	win_w_tt::Matrix{T}
-	win_w_te::Matrix{T}
-	win_w_ee::Matrix{T}
-	fisher::Matrix{T}
-	X_data::Vector{T}
-end
-
-# ╔═╡ f01786a8-5c7a-414b-9506-6002474e79cc
+# ╔═╡ 47f8e2a2-1e09-4cbc-a3ac-6401608f8f6a
 begin
 	LA = ACTLike(
 		np.load("pyactlike_data/win_func_d_tt.npy"),
@@ -64,51 +125,40 @@ begin
 		np.load("pyactlike_data/win_func_w_te.npy"),
 		np.load("pyactlike_data/win_func_w_ee.npy"),
 		np.load("pyactlike_data/fisher.npy"),
-		np.load("pyactlike_data/X_data.npy")
-	)
+		np.load("pyactlike_data/X_data.npy"), 7925)
+	
+	LP = PlanckLike(
+		np.load("pyactlike_data/planck_blmin_TT.npy"),
+		np.load("pyactlike_data/planck_blmax_TT.npy"),
+		np.load("pyactlike_data/planck_bin_w_TT.npy"),
+		np.load("pyactlike_data/planck_blmin.npy"),
+		np.load("pyactlike_data/planck_blmax.npy"),
+		np.load("pyactlike_data/planck_bin_w.npy"),
+		np.load("pyactlike_data/planck_X_data.npy"),
+		np.load("pyactlike_data/planck_fisher.npy"))
 end;
 
-# ╔═╡ 9af2d547-8493-40b1-916f-bee0f4f9604c
-function actlike(L, cltt, clte, clee)
-	yp2 = 1.003
-	lmax_win = 7925
-	cl_tt_d = L.win_d_tt * cltt[2:lmax_win]
-	cl_te_d = L.win_d_te * clte[2:lmax_win]
-	cl_ee_d = L.win_d_ee * clee[2:lmax_win]
-	cl_tt_w = L.win_w_tt * cltt[2:lmax_win]
-	cl_te_w = L.win_w_te * clte[2:lmax_win]
-	cl_ee_w = L.win_w_ee * clee[2:lmax_win]
-	
-	b0 = 5
-	nbintt = 40
-	nbinte = 45
-	nbinee = 45
-	nbin = 260
+# ╔═╡ 4eb6707e-c6ce-4404-9d33-42bac16e53b0
+md"""
+## Model vector
+The second derivative isn't really relevant here, but you do need a way to represent the derivative of your theory with respect to your parameters. I'm working hard on a way to get this directly (see Bolt.jl), but for now I just implement this via a first-order expansion of the theory code, by using finite differencing.
 
-	X_model = [
-		cl_tt_d[b0+1 : b0 + nbintt]
-		cl_te_d[begin:nbinte] * yp2
-		cl_ee_d[begin:nbinee] * yp2 * yp2
-		cl_tt_w[(b0+1) : (b0 + nbintt)]
-		cl_te_w[begin:nbinte] * yp2
-		cl_ee_w[begin:nbinee] * yp2 * yp2
-	]
-	
-	Y = L.X_data .- X_model
-	return -0.5 * (Y' * L.fisher * Y)
-end
+```math
+C_\ell^{th} = C_{\ell}^{th}|_{p_0} + \frac{\Delta C_{\ell}^{th}}{\Delta \theta} (\theta - \theta_0).
+```
 
-# ╔═╡ 3ba7eff3-4f03-472b-a938-1a2dccd2a498
--2 * actlike(LA, cltt, clte, clee) - 288.2530224107163
+Note that this analysis was entirely possible without automatic differentiation, but then I would have to write some complicated Fisher expressions with the binning operators.
+"""
 
-# ╔═╡ 036cc996-e068-4c41-ad26-af0a5a44e466
+# ╔═╡ 8afd3e00-748f-426a-b884-4fc6a9ee35b0
 begin
+	const T_CMB = 2.7255
 	LCDM_pars = (
 		# A_s=2.1824274E-09, 
 		ln_1e10_A_s=log(1e10 * 2.1824274E-09),
 		n_s=9.6437500E-01, theta_s_1e2=1.0419780, 
 		omega_b=2.2283568E-02, tau_reio=7.3211982E-02, omega_dmeff=1.1988179E-01,
-		# sigma_dmeff=0.0
+		sigma_dmeff=0.0
 	)
 	offset_pars = (
 		# A_s=2.1824274E-09 * 1.05, 
@@ -116,20 +166,11 @@ begin
 		n_s=9.6437500E-01 * 1.05, 
 		theta_s_1e2=1.0419780 * 1.05,
 		omega_b=2.2283568E-02 * 1.05, tau_reio=7.3211982E-02 * 1.05, omega_dmeff=1.1988179E-01 * 1.05, 
-		# sigma_dmeff=3.0e-25
+		sigma_dmeff=3.0e-25
 	)
-	
 	Δpar = NamedTuple{keys(LCDM_pars)}(
 		map(k->(offset_pars[k]-LCDM_pars[k]), keys(LCDM_pars)))
-end
-
-# ╔═╡ d5977f2c-83dc-4308-997e-02a34dbb4069
-T_CMB = 2.7255
-
-# ╔═╡ f87f0e25-8fa9-41dd-8070-612721539068
-begin
 	pars = String.(keys(LCDM_pars))
-	print(pars)
 	p0 = Tuple(LCDM_pars)
 	cl_keys = ("lcdm", pars...)
 	
@@ -141,10 +182,6 @@ begin
 			cl_dict["$(k)_$(xy)"] = cl0.get(xy) * (T_CMB * 1e6)^2
 		end
 	end
-end
-
-# ╔═╡ 1fe7b28b-d044-479f-b9e4-f06da9a3bb8e
-begin
 	∂cl = Dict{String, Matrix{Float64}}()
 	for xy in ("tt", "te", "ee")
 	∂cl[xy] = hcat(
@@ -153,51 +190,81 @@ begin
 	end
 end
 
-# ╔═╡ 8d6e2777-5231-4669-9f99-4ba5b89fd54f
-# begin
-# 	clf()
-# 	# plt.plot(cl_dict["A_s_tt"])
-# 	plt.plot(∂cl["tt"][:,4] ./ cl_dict["lcdm_tt"])
-# 	plt.yscale("symlog")
-# 	gcf()
-# end
-
-# ╔═╡ 0be6a6e9-47d7-41e5-a188-f0eb879eb38b
-function get_cl(p, xy)
-	return cl_dict["lcdm_$(xy)"] .+ ∂cl[xy] * collect(p .- p0)
+# ╔═╡ a9b86456-0a89-468d-bb4f-a1ad13107bdb
+begin
+	function get_cl(p, xy)
+		return cl_dict["lcdm_$(xy)"] .+ ∂cl[xy] * collect(p .- p0)
+	end
+	
+	function par2like(p, L::ACTLike, ttlmin=2)
+		lmax_win = L.lmax_win
+		
+		spacer = zeros(lmax_win - 6000) # starts with ℓ=1
+		cltt1 = [zeros(ttlmin - 2); get_cl(p, "tt")[ttlmin:end]; spacer] 
+		clte1= [get_cl(p, "te")[2:end]; spacer]
+		clee1 = [get_cl(p, "ee")[2:end]; spacer]
+	
+		return like(L, cltt1, clte1, clee1)
+	end
+	
+	function par2like(p, L::PlanckLike)
+		lmax_win = 2508
+		cltt1 = get_cl(p, "tt")[3:end]   # starts with ℓ=2
+		clte1= get_cl(p, "te")[3:end]
+		clee1 = get_cl(p, "ee")[3:end]
+	
+		return like(L, cltt1, clte1, clee1)
+	end
 end
 
-# ╔═╡ b2ad25c2-6223-4915-928a-b9097868a986
-function par2like(p, L)
+# ╔═╡ ecdc1673-a65a-4f5c-b820-c21794262468
+md"""
+## Automatic Differentiation (AD) and Fisher
 
-	spacer = zeros(lmax_win - 6000)
-	cltt1 = [get_cl(p, "tt")[2:end]; spacer]
-	clte1= [get_cl(p, "te")[2:end]; spacer]
-	clee1 = [get_cl(p, "ee")[2:end]; spacer]
+Now, let's perform a Fisher analysis of these likelihoods. Here are the parameters.
 
-	return actlike(L, cltt1, clte1, clee1)
-end
+"""
 
-# ╔═╡ 6b94b639-c5c5-4090-ba83-ebafcee47830
-par2like(p0, LA)
-
-# ╔═╡ e136ea2e-9932-4db8-804a-b9c3fd284e43
-fish = -ForwardDiff.hessian(p->par2like(p,LA), collect(p0))
-
-# ╔═╡ 20c737f2-920c-46f0-8765-c5c2f132b958
-fish[5,5] += 1 / (0.015)^2  # tau prior
-
-# ╔═╡ 03e1d5a3-1b9e-4a4b-9e9f-2f2c931b3a2a
+# ╔═╡ 0dde6b0e-fd79-4784-9403-f0a657e830b7
 pars
 
-# ╔═╡ c0d365d3-9059-4079-89d8-009a972b5a06
-sqrt.(diag(inv(fish)))
+# ╔═╡ 9f31c89e-3a24-43c5-aa38-6dea044d1ec1
+md"""
+#### Planck only:
+"""
 
-# ╔═╡ c97e3e28-7f22-4f9c-9078-18d26e356133
-p0
+# ╔═╡ 3d51a515-084a-443e-913a-299495b39eed
+begin
+	fishP = -ForwardDiff.hessian(p->par2like(p, LP), collect(p0))
+	fishP[5,5] += 1 / (0.015)^2  # tau prior to replace lowP
+	sqrt.(diag(inv(fishP)))
+end
 
-# ╔═╡ 76df51e6-5c45-4502-b3f3-411258b22c64
+# ╔═╡ 72abf005-1200-4afe-b654-84f2abfc3f45
+md"""
+#### ACT only:
+"""
 
+# ╔═╡ 14c66b90-7a4d-48c2-bf10-2e3b7fc144d2
+begin
+	fishA = -ForwardDiff.hessian(p->par2like(p, LA), collect(p0))
+	fishA[5,5] += 1 / (0.015)^2  # tau prior to replace lowP
+	sqrt.(diag(inv(fishA)))
+end
+
+# ╔═╡ 464034b6-b75f-4128-8cf8-3fd2d590a75f
+md"""
+#### ACT + Planck:
+"""
+
+# ╔═╡ 917cb116-4ff8-4707-b405-6ff12dccd54d
+begin
+	fishAP_A = -ForwardDiff.hessian(p->par2like(p, LA, 1800), collect(p0))
+	fishAP_P = -ForwardDiff.hessian(p->par2like(p, LP), collect(p0))
+	fishAP = fishAP_A .+ fishAP_P
+	fishAP[5,5] += 1 / (0.015)^2  # tau prior to replace lowP
+	sqrt.(diag(inv(fishAP)))
+end
 
 # ╔═╡ 00000000-0000-0000-0000-000000000001
 PLUTO_PROJECT_TOML_CONTENTS = """
@@ -219,7 +286,7 @@ PLUTO_MANIFEST_TOML_CONTENTS = """
 
 julia_version = "1.8.0-rc1"
 manifest_format = "2.0"
-project_hash = "b6a149abfd952bdecda5b95f00b049fff59ce80a"
+project_hash = "61d1f052e6ce6500139ab07712fd3bb6738471c8"
 
 [[deps.ArgTools]]
 uuid = "0dad84c5-d112-42e6-8d28-ef12dabb789f"
@@ -558,28 +625,22 @@ version = "17.4.0+0"
 """
 
 # ╔═╡ Cell order:
-# ╠═c19e823c-5ee2-4c5e-882f-8611c6decb87
-# ╠═7cd43bd0-6a8a-4d7f-add1-53cebecd79de
-# ╠═c3de8359-835a-461e-8b3c-a6937721dbe4
-# ╠═e4aeb22f-32b0-40fe-bc24-0c1125682aaa
-# ╠═f01786a8-5c7a-414b-9506-6002474e79cc
-# ╠═9af2d547-8493-40b1-916f-bee0f4f9604c
-# ╠═3ba7eff3-4f03-472b-a938-1a2dccd2a498
-# ╠═036cc996-e068-4c41-ad26-af0a5a44e466
-# ╠═d5977f2c-83dc-4308-997e-02a34dbb4069
-# ╠═f87f0e25-8fa9-41dd-8070-612721539068
-# ╠═1fe7b28b-d044-479f-b9e4-f06da9a3bb8e
-# ╠═8d6e2777-5231-4669-9f99-4ba5b89fd54f
-# ╠═0be6a6e9-47d7-41e5-a188-f0eb879eb38b
-# ╠═b2ad25c2-6223-4915-928a-b9097868a986
-# ╠═6b94b639-c5c5-4090-ba83-ebafcee47830
-# ╠═f2a3807f-271c-4049-bb20-46b410bdd5d2
-# ╠═e136ea2e-9932-4db8-804a-b9c3fd284e43
-# ╠═20c737f2-920c-46f0-8765-c5c2f132b958
-# ╠═4dbb0982-c4b8-4502-a3ee-02cba9d57cfa
-# ╠═03e1d5a3-1b9e-4a4b-9e9f-2f2c931b3a2a
-# ╠═c0d365d3-9059-4079-89d8-009a972b5a06
-# ╠═c97e3e28-7f22-4f9c-9078-18d26e356133
-# ╠═76df51e6-5c45-4502-b3f3-411258b22c64
+# ╟─129714fd-46a2-4236-bf90-74b7ed331494
+# ╠═3707bc3c-e79d-11ec-2411-cdcf2d2bb7fc
+# ╟─87b65bd2-aec5-407a-b854-67418f48957c
+# ╠═2c4ef3fd-c8d0-451c-86f6-ee7cd7a40025
+# ╠═47f8e2a2-1e09-4cbc-a3ac-6401608f8f6a
+# ╟─4eb6707e-c6ce-4404-9d33-42bac16e53b0
+# ╠═8afd3e00-748f-426a-b884-4fc6a9ee35b0
+# ╠═a9b86456-0a89-468d-bb4f-a1ad13107bdb
+# ╠═5ee55741-2464-4a03-b357-c1fe00136dbf
+# ╟─ecdc1673-a65a-4f5c-b820-c21794262468
+# ╠═0dde6b0e-fd79-4784-9403-f0a657e830b7
+# ╟─9f31c89e-3a24-43c5-aa38-6dea044d1ec1
+# ╠═3d51a515-084a-443e-913a-299495b39eed
+# ╟─72abf005-1200-4afe-b654-84f2abfc3f45
+# ╠═14c66b90-7a4d-48c2-bf10-2e3b7fc144d2
+# ╟─464034b6-b75f-4128-8cf8-3fd2d590a75f
+# ╠═917cb116-4ff8-4707-b405-6ff12dccd54d
 # ╟─00000000-0000-0000-0000-000000000001
 # ╟─00000000-0000-0000-0000-000000000002
